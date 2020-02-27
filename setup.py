71850e91d0119a40d111c885b2ef1ecd0aecbc17
Will Vining <wfvining@gmail.com>
2020-02-27 14:40:25 -0700

    Configure setuptools (#10)
    * Create setup.py
    
    Sets up the infrastructure for packaging and distributing pvanalytics

---
+++ setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    raise RuntimeError('setuptools is required')
+
+DESCRIPTION = ('PVAnalytics is a python library for the analysis of ' +
+               'photovoltaic system-level data.')
+
+LONG_DESCRIPTION = """
+PVAnalytics is a collection of functions for working with data
+from photovoltaic power systems. The library includes functions for
+general data quality tests such as outlier detection, validation that
+data is physically plausible, filtering data for specific conditions,
+and labeling specific features in the data.
+
+Documentation: https://pvanalytics.readthedocs.io
+
+Source code: https://github.com/pvlib/pvanalytics
+"""
+
+DISTNAME = 'pvanalytics'
+MAINTAINER = "Will Vining"
+MAINTAINER_EMAIL = 'wfvinin@sandia.gov'
+LICENSE = 'MIT'
+URL = 'https://github.com/pvlib/pvanalytics'
+
+TESTS_REQUIRE = [
+    'pytest',
+]
+
+INSTALL_REQUIRES = [
+    'numpy >= 1.16.0',
+    'pandas >= 0.25.1',
+    'pvlib >= 0.7.0'
+]
+
+DOCS_REQUIRE = [
+    'sphinx == 2.2.0'
+]
+
+EXTRAS_REQUIRE = {
+    'test': TESTS_REQUIRE,
+    'doc': DOCS_REQUIRE
+}
+
+EXTRAS_REQUIRE['all'] = sorted(set(sum(EXTRAS_REQUIRE.values(), [])))
+
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering'
+]
+
+PACKAGES = ['pvanalytics']
+
+setup(
+    name=DISTNAME,
+    version="0.0.1",
+    packages=PACKAGES,
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    tests_require=TESTS_REQUIRE,
+    ext_modules=[],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    url=URL
+)
