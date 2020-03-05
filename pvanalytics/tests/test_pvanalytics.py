272c157a6d05cf4346d3ed7e1a505c5a9df128a9
Will Vining <wfvining@gmail.com>
2020-03-05 08:32:36 -0700

    Don't skip the place-holder test
    Skipping tests messes up the coverage reports.

--- pvanalytics/tests/test_pvanalytics.py
+++ pvanalytics/tests/test_pvanalytics.py
@@ -1,6 +1,6 @@
 import pytest
 
 
-@pytest.mark.skip(reason="placeholder for setting up CI pipeline")
 def test_example():
-    assert False
+    """Place-holder test for setting up CI pipelines."""
+    assert True
