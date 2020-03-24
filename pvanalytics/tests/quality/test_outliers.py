d9a6ddeb9c342dddd0a7cc7fc0ba9f6342c9dd9b
Will Vining <wfvining@gmail.com>
2020-03-24 09:41:54 -0600

    Test that we can detect an outlier above the third quartile

--- pvanalytics/tests/quality/test_outliers.py
+++ pvanalytics/tests/quality/test_outliers.py
@@ -13,12 +13,22 @@ def test_iqr_no_outliers():
     )
 
 
-def test_iqr_one_outlier():
-    """filter_iqr properly detects an outlier."""
+def test_iqr_outlier_below():
+    """filter_iqr properly detects an outlier that is too low."""
     data = pd.Series([5, 9, 9, 8, 7, 1, 7, 8, 6, 8])
     assert_series_equal(
         pd.Series([True, True, True, True, True,
                    False, True, True, True, True]),
+        outliers.filter_iqr(data)
+    )
+
+
+def test_iqr_outlier_above():
+    """filter_iqr properly detects an outlier that is too high."""
+    data = pd.Series([0, 1, 3, 2, 1, 3, 4, 1, 1, 9, 2])
+    assert_series_equal(
+        pd.Series([True, True, True, True, True, True,
+                   True, True, True, False, True]),
         outliers.filter_iqr(data)
     )
 
