31aad922d2fc462d2e2c9933119cd25e731f5dc1
Will Vining <wfvining@gmail.com>
2020-04-02 09:31:13 -0600

    Make check_limits quality function public (#25)
    * pull the check_limits function out into a quality.util module
    
    This function is going to be useful for many quality checks and we
    should expose it in our public API as well as for use in other quality
    sub-modules.

---
+++ pvanalytics/quality/util.py
@@ -0,0 +1,51 @@
+"""General-purpose quality control utility functions."""
+import numpy as np
+
+
+def check_limits(val, lower_bound=None, upper_bound=None,
+                 inclusive_lower=False, inclusive_upper=False):
+    """Check whether a value falls withing the given limits.
+
+    At least one of `lower_bound` or `upper_bound` must be provided.
+
+    Parameters
+    ----------
+    val : array_like
+        Values to test.
+    lower_bound : float, default None
+        Lower limit.
+    upper_bound : float, default None
+        Upper limit.
+    inclusive_lower : bool, default False
+        Whether the lower bound is inclusive (`val` >= `lower_bound`).
+    inclusive_upper : bool, default False
+        Whether the upper bound is inclusive (`val` <= `upper_bound`).
+
+    Returns
+    -------
+    array_like
+        True for every value in `val` that is between `lower_bound`
+        and `upper_bound`.
+
+    Raises
+    ------
+    ValueError
+        if `lower_bound` nor `upper_bound` is provided.
+    """
+    if inclusive_lower:
+        lb_op = np.greater_equal
+    else:
+        lb_op = np.greater
+    if inclusive_upper:
+        ub_op = np.less_equal
+    else:
+        ub_op = np.less
+
+    if (lower_bound is not None) & (upper_bound is not None):
+        return lb_op(val, lower_bound) & ub_op(val, upper_bound)
+    elif lower_bound is not None:
+        return lb_op(val, lower_bound)
+    elif upper_bound is not None:
+        return ub_op(val, upper_bound)
+    else:
+        raise ValueError('must provide either upper or lower bound')
---
+++ pvanalytics/tests/quality/test_util.py
@@ -0,0 +1,35 @@
+"""Tests for utility functions."""
+import pandas as pd
+from pandas.util.testing import assert_series_equal
+import pytest
+from pvanalytics.quality import util
+
+
+def test_check_limits():
+    """Test the private check limits function."""
+    expected = pd.Series(data=[True, False])
+    data = pd.Series(data=[3, 2])
+    result = util.check_limits(val=data, lower_bound=2.5)
+    assert_series_equal(expected, result, check_names=False)
+    result = util.check_limits(val=data, lower_bound=3, inclusive_lower=True)
+    assert_series_equal(expected, result, check_names=False)
+
+    data = pd.Series(data=[3, 4])
+    result = util.check_limits(val=data, upper_bound=3.5)
+    assert_series_equal(expected, result, check_names=False)
+    result = util.check_limits(val=data, upper_bound=3, inclusive_upper=True)
+    assert_series_equal(expected, result, check_names=False)
+
+    result = util.check_limits(
+        val=data,
+        lower_bound=3,
+        upper_bound=4,
+        inclusive_lower=True,
+        inclusive_upper=True
+    )
+    assert all(result)
+    result = util.check_limits(val=data, lower_bound=3, upper_bound=4)
+    assert not any(result)
+
+    with pytest.raises(ValueError):
+        util.check_limits(val=data)
