a0e03d95e2837d0390f8023275e25ef2c4614296
Will Vining <wfvining@gmail.com>
2020-04-14 13:58:10 -0600

    Reindex quadratic so vertex falls at index 30
    Previously the vertex was between 30 and 31, this change should make the fixture easier to work with going forward.
    
    Co-Authored-By: Cliff Hansen <cwhanse@sandia.gov>

--- pvanalytics/tests/test_features.py
+++ pvanalytics/tests/test_features.py
@@ -10,10 +10,10 @@ from pvanalytics import features
 def quadratic():
     """Downward facing quadratic.
 
-    Vertex at (30.5, 1000) and roots at 0, 59.
+    Vertex at index 30 and roots at indices 0, 60.
 
     """
-    q = -1000 * (np.linspace(-1, 1, 60) ** 2) + 1000
+    q = -1000 * (np.linspace(-1, 1, 61) ** 2) + 1000
     return pd.Series(q)
 
 
