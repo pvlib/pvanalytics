bb5013d8bd9b1963ff3b72a46c5031b47e04dc3d
Will Vining <wfvining@gmail.com>
2020-04-14 15:00:48 -0600

    Port detect_clipping from Solar Forecast Arbiter (#31)
    * validator.detect_clipping is renamed to features.clipping_levels
    
    In the future we will add different methods for detecting clipping. This naming
    convention matches the convention in pvanalytics.quality as will as in
    pvlib-python.
    
    * simplify tests by removing pvlib dependency
    
    Reimplements a simpler/pared down version of the tests from SFA using
    a quadratic for data rather than generating GHI data using
    pvlib.location.Location.get_clearsky(). Using pvlib to generate testing data 
    significantly complicates our CI process because it requires libraries that
    are not available on the GitHub runners (libhdf5).
    
    * Add parameter defaults from SFA
    
    * Add more complex tests for clipping
    
    The idea is to test data that looks like the summed output of two
    inverters where one inverter is experiencing clipping.
    
    * Add clipping_levels to API documentation
    
    Also adds attribution in licenses.rst
    
    * Apply documentation changes suggested in code review
    
    Co-Authored-By: Cliff Hansen <cwhanse@sandia.gov>
    
    * Test data drops below the clipping level in the middle of clipping
    
    This give two periods of clipping surrounding a period without
    clipping.
    
    * Docstring for 'quadratic' fixture matches index of returned series.
    
    The quadratic describeds in the previous docstring is actually scaled
    and shifted into the index of the series which is the integers [0, 59].
    
    * Reindex quadratic test fixture so vertex falls at index 30
    
    Previously the vertex was between 30 and 31, this change should make the fixture easier to work with going forward.
    
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>

--- pvanalytics/features.py
+++ pvanalytics/features.py
@@ -1,1 +1,74 @@
 """Functions for identifying and labeling features."""
+import pandas as pd
+import numpy as np
+
+
+def _detect_levels(x, count=3, num_bins=100):
+    """Identify plateau levels in data.
+
+    Parameters
+    ----------
+    x : Series
+        Data in which to find plateaus.
+    count : int
+        Number of pleataus to return.
+    num_bins : int
+        Number of bins to use in histogram that finds plateau levels.
+
+    Returns
+    -------
+    list of tuples
+        (left, right) values of the interval in x with a detected
+        plateau, in decreasing order of count of x values in the
+        interval. List length is given by `count`.
+
+    """
+    hist, bin_edges = np.histogram(x, bins=num_bins, density=True)
+    level_index = np.argsort(hist * -1)
+    levels = [(bin_edges[i], bin_edges[i + 1]) for i in level_index[:count]]
+    return levels, bin_edges
+
+
+def _label_clipping(x, window, frac):
+    # label points in a rolling window where the number of 1s is
+    # greater than window*frac
+    tmp = x.rolling(window).sum()
+    y = (tmp >= window * frac) & x.astype(bool)
+    return y
+
+
+def clipping_levels(ac_power, window=4, fraction_in_window=0.75,
+                    rtol=5e-3, levels=2):
+    """Label clipping in AC power data based on levels in the data.
+
+    Parameters
+    ----------
+    ac_power : Series
+        Time series of AC power measurements.
+    window : int, default 4
+        Number of data points in a window used to detect clipping.
+    fraction_in_window : float, default 0.75
+        Fraction of points which indicate clipping if AC power at each
+        point is close to the plateau level.
+    rtol : float, default 5e-3
+        A point is close to a clipped level M if
+        abs(ac_power - M) < rtol * max(ac_power)
+    levels : int, default 2
+        Number of clipped power levels to consider.
+
+    Returns
+    -------
+    Series
+        True when clipping is indicated.
+
+    """
+    num_bins = np.ceil(1.0 / rtol).astype(int)
+    flags = pd.Series(index=ac_power.index, data=False)
+    power_plateaus, bins = _detect_levels(ac_power, count=levels,
+                                          num_bins=num_bins)
+    for lower, upper in power_plateaus:
+        temp = pd.Series(index=ac_power.index, data=0.0)
+        temp.loc[(ac_power >= lower) & (ac_power <= upper)] = 1.0
+        flags = flags | _label_clipping(temp, window=window,
+                                        frac=fraction_in_window)
+    return flags
---
+++ pvanalytics/tests/test_features.py
@@ -0,0 +1,82 @@
+"""Tests for feature labeling functions."""
+import pytest
+import pandas as pd
+from pandas.util.testing import assert_series_equal
+import numpy as np
+from pvanalytics import features
+
+
+@pytest.fixture
+def quadratic():
+    """Downward facing quadratic.
+
+    Vertex at index 30 and roots at indices 0, 60.
+
+    """
+    q = -1000 * (np.linspace(-1, 1, 61) ** 2) + 1000
+    return pd.Series(q)
+
+
+@pytest.fixture
+def quadratic_clipped(quadratic):
+    """Downward facing quadratic with clipping at y=800"""
+    return np.minimum(quadratic, 800)
+
+
+def test_clipping_levels(quadratic, quadratic_clipped):
+    """The clipped segment of a quadratic is properly identified."""
+    expected = quadratic >= 800
+    # because of the rolling window, the first clipped value in the
+    # clipped series will not be marked as clipped (< 1/2 of the
+    # values will be in a different level until there are at least two
+    # clipped values in the window [for window=4])
+    first_true = expected[quadratic >= 800].index[0]
+    expected.loc[first_true] = False
+    assert_series_equal(
+        expected,
+        features.clipping_levels(
+            quadratic_clipped, window=4,
+            fraction_in_window=0.5, levels=4, rtol=5e-3)
+    )
+
+
+def test_clipping_levels_no_clipping(quadratic):
+    """No clipping is identified in a data set that is jsut a quadratic."""
+    assert not features.clipping_levels(
+        quadratic, window=10, fraction_in_window=0.75, levels=4, rtol=5e-3
+    ).any()
+
+
+def test_clipping_levels_compound(quadratic):
+    """No clipping is identified in the sum of two quadratics"""
+    qsum = quadratic + quadratic
+    assert not features.clipping_levels(
+        qsum, window=10, fraction_in_window=0.75, levels=4, rtol=5e-3
+    ).any()
+
+
+def test_clipping_levels_compound_clipped(quadratic, quadratic_clipped):
+    """Clipping is identified in summed quadratics when one quadratic has
+    clipping."""
+    assert features.clipping_levels(quadratic + quadratic_clipped).any()
+
+
+def test_clipping_levels_two_periods(quadratic, quadratic_clipped):
+    """Two periods of clipping with lower values between them.
+
+    The two periods of clipping should be flagged as clipping, and the
+    central period of lower values should not be marked as clipping.
+    """
+    quadratic_clipped.loc[28:31] = [750, 725, 700, 650]
+    clipping = features.clipping_levels(
+        quadratic_clipped,
+        window=4,
+        fraction_in_window=0.5,
+        levels=4,
+        rtol=5e-3
+    )
+    assert not clipping[29:33].any()
+    assert clipping[20:28].all()
+    assert clipping[35:40].all()
+    assert not clipping[0:10].any()
+    assert not clipping[50:].any()
