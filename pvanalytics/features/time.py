5bca54b324dfa4891a3b55a43f630ec6b28b61b2
Will Vining <wfvining@gmail.com>
2020-05-12 15:15:58 -0600

    Threshold-based daytime detection.
    Looks for periods of sufficiently high power or irradiance rather than
    just looking at the frequency of non-zero values. Should be more
    robust to data with non-zero (but low) night time values than
    daytime_frequency, but may suffer from other (as yet unknown
    limitations).

--- pvanalytics/features/time.py
+++ pvanalytics/features/time.py
@@ -8,8 +8,8 @@ def _minute_of_day(time):
     return time.hour * 60 + time.minute
 
 
-def daytime(power_or_irradiance, threshold=0.8, minimum_days=60):
-    """Get the times where the sun is up.
+def daytime_frequency(power_or_irradiance, threshold=0.8, minimum_days=60):
+    """Get the times when the sun is up based on frequency of positive data.
 
     Parameters
     ----------
@@ -46,15 +46,47 @@ def daytime(power_or_irradiance, threshold=0.8, minimum_days=60):
     # group by minute of day and compute frequency of positive values
     # at each minute
     value_frequency = data.groupby(minutes).count()
-
     if value_frequency.max() < minimum_days:
         raise ValueError("Too few days with data (got {}, minimum_days={})"
                          .format(value_frequency.max(), minimum_days))
-    # TODO Ask Matt about the "adaptive" step where the threshold is
-    #      increased from 0.8 to 0.9 if not enough data has been
-    #      excluded.
     daylight_minutes = value_frequency[
         value_frequency > threshold * value_frequency.mean()
     ].index
 
+    return minutes.isin(daylight_minutes)
+
+
+def daytime_level(power_or_irradiance, threshold=0.2, quantile=0.95):
+    """Identify daytime periods based on a threshold.
+
+    Power or irradiance data is aggregated by minute of day and times
+    where the mean power is greater than `threshold` * max are marked
+    as daytime, where max is the `quantile`-percent quantile of the
+    data.
+
+    Parameters
+    ----------
+    power_or_irradiance : Series
+        DatetimeIndexed power or irradiance data.
+    threshold : float, default 0.2
+        Mean power at each minute of the day must be greater than or
+        equal to `threshold` * max where max is the `quantile`-percent
+        quantile of the data for the minute to be considered daytime.
+    quantile : float, default 0.95
+        Quantile to use as the upper bound of the data.
+
+    Returns
+    -------
+    Series
+        A series of booleans with true for timestamps when the sun is
+        up.
+
+    """
+    max_power = power_or_irradiance.quantile(quantile)
+    minutes = pd.Series(_minute_of_day(power_or_irradiance.index),
+                        index=power_or_irradiance.index)
+    mean_by_minute = power_or_irradiance.groupby(minutes).mean()
+    daylight_minutes = mean_by_minute[
+        mean_by_minute >= threshold * max_power
+    ].index
     return minutes.isin(daylight_minutes)
