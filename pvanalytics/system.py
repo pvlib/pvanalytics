370fa438867ba379ab6505a5d704cdb844a4e00c
Will Vining <wfvining@gmail.com>
2020-05-27 11:20:23 -0600

    First cut add curve fitting and orientation inference logic
    Trying to take just the core functionality form PVFleets QA.  These
    are the main differences:
    
    1. Data is not separated into winter and summer months. This means
    that if there are time-shifts in the data they could mess up the
    fit. By removing the separation we get the core
    functionality (curve fitting) and can leave it to the user to
    pass only winter/summer and combine the orientation inferred for each
    in to a meaningful result (i.e. PVFleets basically decides the
    orientation is inconclusive if the winter and summer don't match)
    
    2. A Boolean mask for day/night is passed as a parameter
    
    3. Clipping is passed as a Boolean mask, and the percent of clipping
    is calculated (rather than passing in clip_percent directly)
    
    4. The responsibilities of the function have been made narrower,
    reducing the number of values returned and hopefully making future
    maintenance simpler. It is possible (probable?) that I went too far in
    removing responsibilities and that some additional return values are
    needed. When this comes up we should look carefully at whether to add
    them here or add new functions with separate responsibilities.
    
    Going forward I need to make a testing plan for this and write some
    tests. Also need to decide whether returning None is a good idea, or
    if I should add a third instance to the Orientation
    Enum (e.g. INDETERMINANT).
    
    Finally I would like to do a bit of simplification in the
    _orientation_from_fit function. It would be nice if this could be made
    more general/flexible (i.e. pass in the various thresholds), it will
    take a bit of exploring to figure out the best way forward for this.

--- pvanalytics/system.py
+++ pvanalytics/system.py
@@ -1,48 +1,132 @@
 """Functions for identifying system characteristics."""
-from enum import Enum
+import enum
+import numpy as np
+import scipy.stats
+import scipy.optimize
 
 
-class Orientation(Enum):
+class Orientation(enum.Enum):
     """Orientation of a PV System can be either Fixed or Tracking."""
-    FIXED = auto()
-    TRACKING = auto()
+    FIXED = enum.auto()
+    TRACKING = enum.auto()
 
 
-def orientation(power, daylight, clipping=None):
+def _remove_morning_evening(power, threshold):
+    # Remove morning and evening power by excluding times where
+    # `power` is less than ``threshold * power.max()``
+    return power[power > threshold * power.max()]
+
+
+def _group_by_minute(data):
+    # return data grouped by minute of the day.
+    return data.groupby(data.index.hour * 60 + data.index.minute)
+
+
+def _fit_quadratic(data):
+    # Fit a quadratic to `data` returning R^2 for the fit.
+    coefficients = np.polyfit(data.index, data, 2)
+    # Calculate the R^2 for the fit
+    _, _, correlation, _, _ = scipy.stats.linregress(
+        data, np.poly1d(coefficients)
+    )
+    return correlation**2
+
+
+def _fit_quartic(data, middle):
+    # Fit a quartic to the data.
+    #
+    # The quartic must
+    # - open downwards
+    # - centered within 70 minutes of the middle of `data`
+    # - the y-value of the middle must be within 15% of the median of `data`
+    #
+    # Returns the R^2 for the fit.
+    def quartic(x, a, b, c, e):
+        return a * (x - e)**4 + b * (x - e)**2 + c
+    median = data.median()
+    params, _ = scipy.optimize.curve_fit(
+        quartic,
+        data.index, data,
+        bounds=((-1e-05, 0, median * 0.85, middle - 70),
+                (-1e-10, median * 3e-05, median * 1.15, middle + 70))
+    )
+    model = quartic(data.index, params[0], params[1], params[2], params[3])
+    residuals = data - model
+    return 1 - (np.sum(residuals**2) / np.sum((data - np.mean(data))**2))
+
+
+def _orientation_from_fit(rsquared_quadratic, rsquared_quartic, clip_percent):
+    # Determine orientation based on fit and percent of clipping in the data
+    #
+    # Returns None if orientation cannot be determined, otherwise
+    # returns the orientation
+    if clip_percent < 0.5:
+        if rsquared_quadratic >= 0.945:
+            return Orientation.FIXED
+        if rsquared_quartic >= 0.945 and rsquared_quadratic < 0.92:
+            return Orientation.TRACKING
+    elif clip_percent <= 3.0:
+        if rsquared_quadratic >= 0.92:
+            return Orientation.FIXED
+        if rsquared_quartic >= 0.92 and rsquared_quadratic < 0.92:
+            return Orientation.TRACKING
+    elif clip_percent <= 4.0:
+        if rsquared_quadratic >= 0.90:
+            return Orientation.FIXED
+        if rsquared_quartic >= 0.92 and rsquared_quadratic < 0.92:
+            return Orientation.TRACKING
+    elif clip_percent <= 10.0:
+        if rsquared_quadratic >= 0.88:
+            return Orientation.FIXED
+        if rsquared_quartic >= 0.92 and rsquared_quadratic < 0.92:
+            return Orientation.TRACKING
+    return None
+
+
+def orientation(power, daytime, clipping, fit_median=True):
     """Infer the orientation of the system from power data.
 
     Parameters
     ----------
     power : Series
         Time series of AC power data.
-    daylight : Series
+    daytime : Series
         Boolean Series with True for times that are during the day.
-    clipping : Series, default None
+    clipping : Series
         Boolean Series identifying where power is being clipped.
+    fit_median : boolean, default True
+        Perform a secondary fit with the median power to validate that
+        the orientation is consistent through the entire data set.
 
     Returns
     -------
-    Orientation or None
+    orientation : Orientation or None
         If the orientation could not be determined returns None,
         otherwise returns the inferred orientation.
 
     """
-    # group data by minute and compute the 99.5% quantile.
-    daytime_power = power[daylight]
-    power_by_minute = daytime_power.groupby(
-        power.index.hour * 60 + power.index.minute
+    envelope = _remove_morning_evening(
+        _group_by_minute(power[daytime]).quantile(0.995),
+        0.05
     )
-    by_minute_quantile = power_by_minute.quantile(0.995)
-    by_minute_median = power_by_minute.median()
-    # remove low values (these would be morning and evening times)
-    upper_envelope = by_minute_quantile[
-        by_minute_quantile > 0.05 * by_minute_quantile.max()
-    ]
-    median_envelope = by_minute_median[
-        by_minute_median > 0.025 * by_minute_median.max()
-    ]
-    quantile_middle = (upper_envelope.index.max()
-                       + upper_envelope.index.min()) / 2
-    # try different curve fitting to identify the orientation.
-    # TODO
-    return None
+    middle = (envelope.index.max() + envelope.index.min()) / 2
+    rsquared_quadratic = _fit_quadratic(envelope)
+    rsquared_quartic = _fit_quartic(envelope, middle)
+    system_orientation = _orientation_from_fit(
+        rsquared_quadratic, rsquared_quartic,
+        (clipping[daytime].sum() / len(clipping[daytime])) * 100
+    )
+    if fit_median:
+        median = _remove_morning_evening(
+            _group_by_minute(power[daytime]).median(),
+            0.025
+        )
+        if system_orientation is Orientation.FIXED:
+            quadratic_median = _fit_quadratic(median)
+            if quadratic_median < 0.9:
+                return None
+        elif system_orientation is Orientation.TRACKING:
+            quartic_median = _fit_quartic(median, middle)
+            if quartic_median < 0.9:
+                return None
+    return system_orientation
