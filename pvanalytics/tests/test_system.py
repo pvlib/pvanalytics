6043dbdad92f24443536db5de501f1253a8a41b7
Will Vining <wfvining@gmail.com>
2020-06-01 10:20:15 -0600

    Add orientation test based on simulated power data
    Uses pvlib to simulate power from a fixed PV system and verifies that
    system.orientation() returns Orientation.FIXED.

--- pvanalytics/tests/test_system.py
+++ pvanalytics/tests/test_system.py
@@ -1,11 +1,12 @@
 """Tests for funcitons that identify system characteristics."""
 import pytest
 import pandas as pd
-from pvlib import location
+from pvlib import location, pvsystem, modelchain
+from pvlib.temperature import TEMPERATURE_MODEL_PARAMETERS
 from pvanalytics import system
 
 
-# Rought testing plan
+# Rough testing plan
 #
 # Generate several data sets (winter/summer only) using PVLib with
 # different orientation and other characteristics and validate
@@ -16,7 +17,7 @@ from pvanalytics import system
 
 # TODO Generate data using pvlib.tracking.SingleAxisTracker (TRACKING)
 
-# TODO Generate power data from a PVSystem without a tracker
+# TODO Address data with wrong timezone (midnight is during th day)
 
 
 @pytest.fixture
@@ -33,7 +34,7 @@ def summer_times():
 
 @pytest.fixture
 def albuquerque():
-    """pvlib location for Albuquerque, NM."""
+    """pvlib Location for Albuquerque, NM."""
     return location.Location(
         35.0844,
         -106.6504,
@@ -44,10 +45,40 @@ def albuquerque():
 
 
 @pytest.fixture
+def system_parameters():
+    """System parameters for generating simulated power data."""
+    sandia_modules = pvsystem.retrieve_sam('SandiaMod')
+    sapm_inverters = pvsystem.retrieve_sam('cecinverter')
+    module = sandia_modules['Canadian_Solar_CS5P_220M___2009_']
+    inverter = sapm_inverters['ABB__MICRO_0_25_I_OUTD_US_208__208V_']
+    temperature_model_parameters = (
+        TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
+    )
+    return {
+        'module_parameters': module,
+        'inverter_parameters': inverter,
+        'temperature_model_parameters': temperature_model_parameters
+    }
+
+
+@pytest.fixture
 def summer_ghi(summer_times, albuquerque):
     """Clearsky GHI for Summer, 2020 in Albuquerque, NM."""
     clearsky = albuquerque.get_clearsky(summer_times)
     return clearsky['ghi']
+
+
+@pytest.fixture
+def summer_power_fixed(summer_times, albuquerque, system_parameters):
+    """Simulated power from a FIXED PVSystem in Albuquerque, NM."""
+    system = pvsystem.PVSystem(**system_parameters)
+    mc = modelchain.ModelChain(
+        system,
+        albuquerque,
+        orientation_strategy='south_at_latitude_tilt'
+    )
+    mc.run_model(albuquerque.get_clearsky(summer_times))
+    return mc.ac
 
 
 def test_ghi_orientation_fixed(summer_ghi):
@@ -56,4 +87,13 @@ def test_ghi_orientation_fixed(summer_ghi):
         summer_ghi,
         summer_ghi > 0,
         pd.Series(False, index=summer_ghi.index)
+    ) is system.Orientation.FIXED
+
+
+def test_power_orientation_fixed(summer_power_fixed):
+    """Simulated system under clearsky condidtions is FIXED."""
+    assert system.orientation(
+        summer_power_fixed,
+        summer_power_fixed > 0,
+        pd.Series(False, index=summer_power_fixed.index)
     ) is system.Orientation.FIXED
