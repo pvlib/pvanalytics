af5761fe516af00c50a63787d6e03a5d2af5b355
PVPro-SPA <63314623+PVPro-SPA@users.noreply.github.com>
2020-08-26 14:45:14 -0400

    Update NREL_performance_ratio.py
    Updated citations and descriptions in docstrings

--- pvanalytics/NREL_performance_ratio.py
+++ pvanalytics/NREL_performance_ratio.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 5 14:51:04 2020
-
-@author: Saurabh
-"""
 from pvlib.temperature import sapm_cell
 from pvlib.pvsystem import pvwatts_dc
 
@@ -13,7 +7,7 @@ def get_performance_ratio(poa_global, temp_air, wind_speed, pac, pdc0, a=-3.56,
     r'''
     Calculate NREL Performance Ratio.
 
-    See equation [5] in Weather-Corrected Performance Ratio for details on 
+    See equation [5] in Weather-Corrected Performance Ratio [1]_ for details on 
     the weighted method for Tref.
 
     Parameters
@@ -27,34 +21,35 @@ def get_performance_ratio(poa_global, temp_air, wind_speed, pac, pdc0, a=-3.56,
     wind_speed : numeric
         Wind speed at a height of 10 meters [m/s].
 
-    pac : numeric
+    pac : float
         AC power [kW].
 
-    pdc0 : numeric
-        Power of the modules at 1000 W/m2 and cell reference temperature.
+    pdc0 : float
+        Power of the modules at 1000 W/m2 and cell reference temperature [kW].
         
     a : float
-        Parameter :math:`a` in :eq:`sapm1mod`.
+        Parameter :math:`a` in SAPM model [unitless].
 
     b : float
-        Parameter :math:`b` in :eq:`sapm1mod`.
+        Parameter :math:`b` in in SAPM model [s/m].
         
     deltaT : float
-        Parameter :math:`\Delta T` in :eq:`sapm2` [C].
+        Parameter :math:`\Delta T` in SAPM model [C].
 
-    gamma_pdc : numeric
+    gamma_pdc : float
                 The temperature coefficient in units of 1/C. Typically -0.002 
-                to -0.005 per degree C.
+                to -0.005 per degree C [1/C].
 
     Returns
     -------
-    performance_ratio: numeric
+    performance_ratio: float
         Performance Ratio of data.
 
 
     References
     ----------
-    .. [1] "Weather-Corrected Performance Ratio". NREL, 2013. 
+    .. [1] Dierauf et al. "Weather-Corrected Performance Ratio". NREL, 2013. 
+       https://www.nrel.gov/docs/fy13osti/57991.pdf
     '''
     
     cell_temperature = sapm_cell(poa_global, 
