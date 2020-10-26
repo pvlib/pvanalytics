0990a73c812d47c40b677ecd9f02736d503b1701
Will Vining <wfvining@gmail.com>
2020-10-26 11:06:44 -0600

    Improve documentation for freq_to_timedelta

--- pvanalytics/util/_functions.py
+++ pvanalytics/util/_functions.py
@@ -4,14 +4,15 @@ from pandas.tseries import frequencies
 
 
 def freq_to_timedelta(freq):
-    """Convert a pandas freqstr to a timedelta
+    """Convert a pandas freqstr to a Timedelta.
 
     Parameters
     ----------
     freq : str
-
+        A pandas freqstr (e.g. '10T').
     Returns
     -------
     Timedelta
+        Timedelta corresponding to a single period at `freq`.
     """
     return pd.to_timedelta(frequencies.to_offset(freq))
