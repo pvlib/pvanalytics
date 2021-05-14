ecf8709a9fc07a626208f02674dbf82822b0136f
Michael Hopwood <michael.hopwood@knights.ucf.edu>
2021-05-14 17:41:51 -0400

    Update pvanalytics/quality/outliers.py
    Co-authored-by: Will Vining <wfvining@gmail.com>

--- pvanalytics/quality/outliers.py
+++ pvanalytics/quality/outliers.py
@@ -71,7 +71,7 @@ def zscore(data, zmax=1.5, nan_policy='raise'):
         elif nan_policy == 'omit':
             nan_mask = data.isna()
         else:
-            raise ValueError("Incorrect specification passed " +
+            raise ValueError("Incorrect specification passed "
                              "to zscore's nan_policy.")
 
     data[~nan_mask] = pd.Series(abs(stats.zscore(data[~nan_mask])) > zmax,
