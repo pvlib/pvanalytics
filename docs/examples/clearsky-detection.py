5f7bcfb3405b1095e7563f92c25c27f32b8cc761
Kevin Anderson <kevin.anderson@nrel.gov>
2022-01-27 18:13:33 -0500

    nix network request in example

--- docs/examples/clearsky-detection.py
+++ docs/examples/clearsky-detection.py
@@ -9,21 +9,29 @@ Identifying periods of clear-sky conditions using measured irradiance.
 # Identifying and filtering for clear-sky conditions is a useful way to
 # reduce noise when analyzing measured data.  This example shows how to
 # use :py:func:`pvanalytics.features.clearsky.reno` to identify clear-sky
-# conditions using measured GHI data.  For this example we'll retrieve
+# conditions using measured GHI data.  For this example we'll use
 # GHI measurements from NREL in Golden CO.
 
+import pvanalytics
 from pvanalytics.features.clearsky import reno
-from pvlib.iotools import read_midc_raw_data_from_nrel
 import pvlib
 import matplotlib.pyplot as plt
 import pandas as pd
+import pathlib
 
 # %%
-# First, fetch some example GHI data using pvlib.  For this example we'll just
-# use a single day, but the same process applies to data of any length.
+# First, read in the GHI measurements.  For this example we'll use an example
+# file included in pvanalytics covering a single day, but the same process
+# applies to data of any length.
 
-date = pd.to_datetime('2022-01-20')
-data = read_midc_raw_data_from_nrel('BMS', start=date, end=date)
+pvanalytics_dir = pathlib.Path(pvanalytics.__file__).parent
+ghi_file = pvanalytics_dir / 'data' / 'midc_bms_ghi_20220120.csv'
+data = pd.read_csv(ghi_file, index_col=0, parse_dates=True)
+
+# or you can fetch the data straight from the source using pvlib:
+# date = pd.to_datetime('2022-01-20')
+# data = pvlib.iotools.read_midc_raw_data_from_nrel('BMS', date, date)
+
 measured_ghi = data['Global CMP22 (vent/cor) [W/m^2]']
 
 # %%
