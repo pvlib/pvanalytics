7dfa9d911b3409f831a467eccaf1ad1dac042953
Perry <kperry@nrel.gov>
2022-02-21 12:24:44 -0700

    added sphinx documentation + examples for running the clipping mask.

--- docs/examples/clipping.py
+++ docs/examples/clipping.py
@@ -21,23 +21,53 @@ from pvanalytics.features.clipping import geometric
 import matplotlib.pyplot as plt
 import pandas as pd
 import pathlib
+import numpy as np
 
 # %%
-# First, read in the ac_power_inv_2054 example, and visualize clipping
-# periods via the "label" mask column.
+# First, read in the ac_power_inv_7539 example, and visualize a subset of the
+# clipping periods via the "label" mask column.
 
 pvanalytics_dir = pathlib.Path(pvanalytics.__file__).parent
-ac_power_file_1 = pvanalytics_dir / 'data' / 'ac_power_inv_2054.csv'
-data = pd.read_csv(ac_power_file_1, index_col=0, parse_dates=True)
+ac_power_file_1 = pvanalytics_dir / 'data' / 'ac_power_inv_7539.csv'
+data = pd.read_csv(ac_power_file_1, index_col=3, parse_dates=True)
+data['label'] = data['label'].astype(bool)
+freq = str(int(data.index.to_series().diff().value_counts().idxmax()
+               .seconds/60))+"T"
+scatter = plt.scatter(data.index[3500:4000],
+                      data['value_normalized'][3500:4000],
+                      c=data.label[3500:4000].astype('int'),
+                      cmap="bwr")
+plt.legend(handles=scatter.legend_elements()[0],
+           labels=[False, True],
+           title="Clipped")
+plt.xticks(rotation=45)
+plt.xlabel("Date", size=18)
+plt.ylabel("Normalized AC Power", size=18)
+plt.show()
+plt.close()
 
 # %%
 # Now, use :py:func:`pvanalytics.features.clipping.geometric` to identify
-# clipping periods in the time series. Re-plot the data with this mask.
+# clipping periods in the time series. Re-plot the data subset with this mask.
+predicted_clipping_mask = geometric(ac_power=data['value_normalized'],
+                                    freq=freq)
+scatter = plt.scatter(data.index[3500:4000],
+                      data['value_normalized'][3500:4000],
+                      c=predicted_clipping_mask[3500:4000].astype('int'),
+                      cmap="bwr")
+plt.legend(handles=scatter.legend_elements()[0],
+           labels=[False, True],
+           title="Clipped")
+plt.xticks(rotation=45)
+plt.xlabel("Date", size=18)
+plt.ylabel("Normalized AC Power", size=18)
+plt.show()
+plt.close()
 
 
 # %%
 # Compare the filter results to the ground-truth labeled data side-by-side,
-# and generate accuracy and F1-score metrics.
-
-
-
+# and generate an accuracy metric.
+acc = 100 * np.sum(np.equal(data.label,
+                            predicted_clipping_mask))/len(data.label)
+print("Overall model prediction accuracy: " + str(round(acc, 2)) + "%")
