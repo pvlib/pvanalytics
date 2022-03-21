64a2a8c20e17befe24536740152ba40b8f0b584a
Perry <kperry@nrel.gov>
2022-03-21 16:56:52 -0600

    update the day-night masking routine.

--- docs/examples/day-night-masking.py
+++ docs/examples/day-night-masking.py
@@ -6,20 +6,22 @@ Masking day-night periods using the PVAnalytics daytime module.
 """
 
 # %%
-# Identifying and masking day-night periods in an AC power time series or 
-# irradiance time series can aid in future data analysis, such as detecting if
-# a time series has daylight savings time or time shifts. 
+# Identifying and masking day-night periods in an AC power time series or
+# irradiance time series can aid in future data analysis, such as detecting
+# if a time series has daylight savings time or time shifts.
 
 import pvanalytics
 from pvanalytics.features.daytime import power_or_irradiance
 import matplotlib.pyplot as plt
 import pandas as pd
 import pathlib
-import numpy as np
 
 # %%
-# First, read in the ac_power_inv_7539 example, and visualize a subset of the
-# clipping periods via the "label" mask column.
+# First, read in the XXX example.
+# This sample is provided from the NREL PVDAQ database, and contains
+# a column representing an AC power data stream and a day-night
+# mask column. Data associated with daytime periods is labeled as True,
+# and data associated with nighttime periods is labeled as False.
 
 pvanalytics_dir = pathlib.Path(pvanalytics.__file__).parent
 ac_power_file_1 = "C:/Users/kperry/Documents/source/repos/pvanalytics/pvanalytics/data/ac_power_inv_7539.csv"#pvanalytics_dir / 'data' / 'ac_power_inv_7539.csv'
@@ -38,16 +40,17 @@ plt.tight_layout()
 plt.show()
 
 # %%
-# Now, use :py:func:`pvanalytics.features.clipping.geometric` to identify
-# clipping periods in the time series. Re-plot the data subset with this mask.
+# Now, use :py:func:`pvanalytics.features.daytime.power_or_irradiance`
+# to identify day periods in the time series. Re-plot the data
+# subset with this mask.
 predicted_day_night_mask = power_or_irradiance(series=data['value_normalized'],
                                                freq=freq)
 data['value_normalized'].plot()
 data.loc[predicted_day_night_mask, 'value_normalized'].plot(ls='', marker='o')
-plt.legend(labels=["AC Power", "Daytime"])
+data.loc[~predicted_day_night_mask, 'value_normalized'].plot(ls='', marker='o')
+plt.legend(labels=["AC Power", "Daytime", "Nighttime"])
 plt.xticks(rotation=20)
 plt.xlabel("Date")
 plt.ylabel("Normalized AC Power")
 plt.tight_layout()
 plt.show()
-
