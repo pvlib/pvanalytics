8fca43cd2428c7d279072138e79cdd7043e47b62
Perry <kperry@nrel.gov>
2022-03-29 12:50:20 -0600

    Made updates to documentation based on @kanderso-nrel's recommendations.

--- docs/examples/day-night-masking.py
+++ docs/examples/day-night-masking.py
@@ -8,7 +8,11 @@ Masking day-night periods using the PVAnalytics daytime module.
 # %%
 # Identifying and masking day-night periods in an AC power time series or
 # irradiance time series can aid in future data analysis, such as detecting
-# if a time series has daylight savings time or time shifts.
+# if a time series has daylight savings time or time shifts. Here, we use
+# :py:func:`pvanalytics.features.daytime.power_or_irradiance` to mask day/night
+# periods, as well as estimate sunrise and sunset times in the data set. This
+# function is particularly useful for cases where the time zone of a data
+# stream is unknown or incorrect.
 
 import pvanalytics
 from pvanalytics.features.daytime import power_or_irradiance
@@ -37,18 +41,33 @@ latitude = 39.742
 longitude = -105.173
 # Plot the time series.
 data['ac_power__752'].plot()
-plt.xticks(rotation=20)
 plt.xlabel("Date")
 plt.ylabel("AC Power (kW)")
 plt.tight_layout()
 plt.show()
 
 # %%
-# The pvlib.solarposition.sun_rise_set_transit_spa function is
-# used to get sunrise and sunset times for each day at the site location, and
-# the daytime mask is calculated based on these times. Data associated with
-# day time periods is labeled as True, and data associated with nighttime
-# periods is labeled as False.
+# It is critical to set all negative values in the AC power time series to 0
+# for :py:func:`pvanalytics.features.daytime.power_or_irradiance` to work
+# properly. Negative erroneous data may affect daytime mask assignments.
+data.loc[data['ac_power__752'] < 0, 'ac_power__752'] = 0
+
+# %%
+# Now, use :py:func:`pvanalytics.features.daytime.power_or_irradiance`
+# to identify day periods in the time series.
+predicted_day_night_mask = power_or_irradiance(series=data['ac_power__752'],
+                                               freq=freq)
+
+# %%
+# Function :py:func:`pvlib.solarposition.sun_rise_set_transit_spa` is
+# used to get ground-truth sunrise and sunset times for each day at the site
+# location, and a SPA-daytime mask is calculated based on these times. Data
+# associated with SPA daytime periods is labeled as True, and data associated
+# with SPA nighttime periods is labeled as False.
+# SPA sunrise and sunset times are used here as a point of comparison to the
+# sensor-based AC power data; SPA-based sunrise and sunset values are not
+# needed to run :py:func:`pvanalytics.features.daytime.power_or_irradiance`.
+
 sunrise_sunset_df = pvlib.solarposition.sun_rise_set_transit_spa(data.index,
                                                                  latitude,
                                                                  longitude,
@@ -61,16 +80,12 @@ data['daytime_mask'] = True
 data.loc[(data.index < data.sunrise_time) |
          (data.index > data.sunset_time), "daytime_mask"] = False
 
-# %%
-# Set all negative values in the AC power time series to 0.
-data.loc[data['ac_power__752'] < 0, 'ac_power__752'] = 0
 
 # %%
-# Now, use :py:func:`pvanalytics.features.daytime.power_or_irradiance`
-# to identify day periods in the time series. Re-plot the data
-# subset with this mask, as well as the SPA-calculate sunrise and sunset
-predicted_day_night_mask = power_or_irradiance(series=data['ac_power__752'],
-                                               freq=freq)
+# Plot the Ac power data stream with the mask output from
+# :py:func:`pvanalytics.features.daytime.power_or_irradiance`,
+# as well as the SPA-calculate sunrise and sunset
+
 data['ac_power__752'].plot()
 data.loc[predicted_day_night_mask, 'ac_power__752'].plot(ls='', marker='o')
 data.loc[~predicted_day_night_mask, 'ac_power__752'].plot(ls='', marker='o')
@@ -81,100 +96,13 @@ for sunrise, sunset in sunrise_sunset_times.itertuples(index=False):
     plt.axvline(x=sunset, c="red")
 plt.legend(labels=["AC Power", "Daytime", "Nighttime",
                    "SPA Sunrise", "SPA Sunset"])
-plt.xticks(rotation=20)
 plt.xlabel("Date")
 plt.ylabel("AC Power (kW)")
 plt.tight_layout()
 plt.show()
 
 # %%
-# Compare the predicted mask to the ground-truth mask, to get the model
-# accuracy. Also, compare sunrise and sunset times for the predicted mask
-# compared to the ground truth sunrise and sunset times.
-acc = 100 * np.sum(np.equal(data.daytime_mask,
-                            predicted_day_night_mask))/len(data.daytime_mask)
-print("Overall model prediction accuracy: " + str(round(acc, 2)) + "%")
-
-# Generate predicted + SPA sunrise times for each day
-print("Sunrise Comparison:")
-print(pd.DataFrame({'predicted_sunrise': predicted_day_night_mask
-                    .index[predicted_day_night_mask]
-                    .to_series().resample("d").first(),
-                    'pvlib_spa_sunrise': sunrise_sunset_df["sunrise"]
-                    .resample("d").first()}))
-# Generate predicted + SPA sunset times for each day
-print("Sunset Comparison:")
-print(pd.DataFrame({'predicted_sunset': predicted_day_night_mask
-                    .index[predicted_day_night_mask]
-                    .to_series().resample("d").last(),
-                    'pvlib_spa_sunset': sunrise_sunset_df["sunrise"]
-                    .resample("d").last()}))
-
-# %%
-# Now we repeat the above process with 15-minute sampled AC power time series
-# data, taken from the SERF East installation on the NREL campus. This data
-# is derived from the 1-minute data above, except it has been resampled to
-# 15-minute right-aligned mean data.
-ac_power_file = pvanalytics_dir / 'data' / 'serf_east_15min_ac_power.csv'
-data = pd.read_csv(ac_power_file, index_col=0, parse_dates=True)
-data = data.sort_index()
-# This is the known frequency of the time series. You may need to infer
-# the frequency or set the frequency with your AC power time series.
-freq = "15T"
-# Plot the time series.
-data['ac_power__752'].plot()
-plt.xticks(rotation=20)
-plt.xlabel("Date")
-plt.ylabel("AC Power (kW)")
-plt.tight_layout()
-plt.show()
-
-# %%
-# The pvlib.solarposition.sun_rise_set_transit_spa function is
-# used to get sunrise and sunset times for each day at the site location, and
-# the daytime mask is calculated based on these times. Data associated with
-# day time periods is labeled as True, and data associated with nighttime
-# periods is labeled as False.
-sunrise_sunset_df = pvlib.solarposition.sun_rise_set_transit_spa(data.index,
-                                                                 latitude,
-                                                                 longitude,
-                                                                 how='numpy',
-                                                                 delta_t=67.0)
-data['sunrise_time'] = sunrise_sunset_df['sunrise']
-data['sunset_time'] = sunrise_sunset_df['sunset']
-
-data['daytime_mask'] = True
-data.loc[(data.index < data.sunrise_time) |
-         (data.index > data.sunset_time), "daytime_mask"] = False
-
-# %%
-# Set all negative values in the AC power time series to 0.
-data.loc[data['ac_power__752'] < 0, 'ac_power__752'] = 0
-
-# %%
-# Now, use :py:func:`pvanalytics.features.daytime.power_or_irradiance`
-# to identify day periods in the time series. Re-plot the data
-# subset with this mask.
-predicted_day_night_mask = power_or_irradiance(series=data['ac_power__752'],
-                                               freq=freq)
-data['ac_power__752'].plot()
-data.loc[predicted_day_night_mask, 'ac_power__752'].plot(ls='', marker='o')
-data.loc[~predicted_day_night_mask, 'ac_power__752'].plot(ls='', marker='o')
-sunrise_sunset_times = sunrise_sunset_df[['sunrise',
-                                          'sunset']].drop_duplicates()
-for sunrise, sunset in sunrise_sunset_times.itertuples(index=False):
-    plt.axvline(x=sunrise, c="blue")
-    plt.axvline(x=sunset, c="red")
-plt.legend(labels=["AC Power", "Daytime", "Nighttime",
-                   "SPA Sunrise", "SPA Sunset"])
-plt.xticks(rotation=20)
-plt.xlabel("Date")
-plt.ylabel("AC Power (kW)")
-plt.tight_layout()
-plt.show()
-
-# %%
-# Compare the predicted mask to the ground-truth mask, to get the model
+# Compare the predicted mask to the ground-truth SPA mask, to get the model
 # accuracy. Also, compare sunrise and sunset times for the predicted mask
 # compared to the ground truth sunrise and sunset times.
 acc = 100 * np.sum(np.equal(data.daytime_mask,
