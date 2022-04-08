cc874e05c405393af32a79cb53d5daa5bf988e9f
Kirsten Perry <70228568+kperrynrel@users.noreply.github.com>
2022-04-08 16:21:53 -0600

    Update docs/examples/clearsky-limits-irradiance.py
    Co-authored-by: Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>

--- docs/examples/clearsky-limits-irradiance.py
+++ docs/examples/clearsky-limits-irradiance.py
@@ -8,7 +8,7 @@ Checking the clearsky limits of irradiance data.
 # %%
 # Identifying and filtering out invalid irradiance data is a
 # useful way to reduce noise during analysis. In this example,
-# we use :py:func:`pvanalytics.quality.irradiance.clearksy_limits`
+# we use :py:func:`pvanalytics.quality.irradiance.clearsky_limits`
 # to identify irradiance values that do not exceed
 # clearsky values. For this example we will use the RMIS weather
 # system located on the NREL campus in CO.
