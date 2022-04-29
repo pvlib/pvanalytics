37a43a58453e3b67d457fd67106083ed7210638f
Kirsten Perry <70228568+kperrynrel@users.noreply.github.com>
2022-04-29 15:47:50 -0600

    Update docs/examples/clearsky-limits-irradiance.py
    Co-authored-by: Cliff Hansen <cwhanse@sandia.gov>

--- docs/examples/clearsky-limits-irradiance.py
+++ docs/examples/clearsky-limits-irradiance.py
@@ -21,9 +21,9 @@ import pandas as pd
 import pathlib
 
 # %%
-# First, read in the RMIS NREL system. This data set contains
-# 5-minute right-aligned data. It includes POA, GHI,
-# DNI, DHI, and GNI measurements, but only the GHI is relevant here.
+# First, read in data from the RMIS NREL system. This data set contains
+# 5-minute right-aligned POA, GHI, DNI, DHI, and GNI measurements,
+# but only the GHI is relevant here.
 
 pvanalytics_dir = pathlib.Path(pvanalytics.__file__).parent
 rmis_file = pvanalytics_dir / 'data' / 'irradiance_RMIS_NREL.csv'
