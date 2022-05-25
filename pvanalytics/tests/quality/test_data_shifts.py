334b5d50a95e66ecf2df48bd9bf22819c0232cf2
Kirsten Perry <kperry@nrel.gov>
2022-05-25 11:42:36 -0600

    added unit test for error throw for weekly time series.

--- pvanalytics/tests/quality/test_data_shifts.py
+++ pvanalytics/tests/quality/test_data_shifts.py
@@ -5,18 +5,19 @@ from pvanalytics.quality import data_shifts as dt
 from ..conftest import DATA_DIR
 import ruptures
 
-test_file_1 = DATA_DIR / "pvlib_data_shift_stream_example_1.csv"
-
+test_file_1 = DATA_DIR / "pvlib_data_shift.csv"
 
 @pytest.fixture
-def generate_daily_time_series():
+def generate_time_series():
     # Pull down the saved PVLib dataframe and process it
     df = pd.read_csv(test_file_1)
     signal_no_index = df['value']
     df.index = pd.to_datetime(df['timestamp'])
     signal_datetime_index = df['value']
     changepoint_date = df[df['label'] == 1].index[0]
-    return signal_no_index, signal_datetime_index, changepoint_date
+    df_weekly_resample = df.resample('W').median()['value']
+    return (signal_no_index, signal_datetime_index,
+            df_weekly_resample, changepoint_date)
 
 
 def requires_ruptures(test):
@@ -36,14 +37,16 @@ def test_detect_data_shifts(generate_daily_time_series):
     Unit test that data shifts are correctly identified in the simulated time
     series.
     """
-    signal_no_index, signal_datetime_index, changepoint_date = \
-        generate_daily_time_series
+    signal_no_index, signal_datetime_index, df_weekly_resample, \
+        changepoint_date = generate_daily_time_series
     # Test that an error is thrown when a Pandas series with no datetime
     # index is passed
     pytest.raises(TypeError, dt.detect_data_shifts, signal_no_index)
     # Test that an error is thrown when an incorrect ruptures method is passed
     pytest.raises(TypeError, dt.detect_data_shifts, signal_datetime_index,
                   True, False, "Pelt")
+    # Test that an error is thrown when weekly data is passed
+    pytest.raises(ValueError, dt.detect_data_shifts, df_weekly_resample)
     # Test that an error is thrown when an incorrect string is passed as the
     # cost variable
     pytest.raises(ValueError, dt.detect_data_shifts, signal_datetime_index,
@@ -73,8 +76,8 @@ def test_filter_data_shifts(generate_daily_time_series):
     Unit test that the longest interval between data shifts is selected for
     the simulated daily time series data set.
     """
-    signal_no_index, signal_datetime_index, changepoint_date = \
-        generate_daily_time_series
+    signal_no_index, signal_datetime_index, df_weekly_resample, \
+        changepoint_date = generate_daily_time_series
     # Run the time series where there are no changepoints
     interval_dict_short = dt.get_longest_shift_segment_dates(
         time_series=signal_datetime_index[:100])
