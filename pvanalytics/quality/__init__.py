4b847e71083b0135b988da79cf57c05f5b4f95b6
Kirsten Perry <kperry@nrel.gov>
2022-05-25 14:04:12 -0600

    Debug pep8 fixes.

--- pvanalytics/quality/__init__.py
+++ pvanalytics/quality/__init__.py
@@ -1,7 +1,7 @@
-from pvanalytics.quality import gaps        # noqa: F401
-from pvanalytics.quality import util        # noqa: F401
-from pvanalytics.quality import irradiance  # noqa: F401
-from pvanalytics.quality import weather     # noqa: F401
-from pvanalytics.quality import outliers    # noqa: F401
-from pvanalytics.quality import time        # noqa: F401
-from pvanalytics.quality import data_shifts # noqa: F401
+from pvanalytics.quality import gaps         # noqa: F401
+from pvanalytics.quality import util         # noqa: F401
+from pvanalytics.quality import irradiance   # noqa: F401
+from pvanalytics.quality import weather      # noqa: F401
+from pvanalytics.quality import outliers     # noqa: F401
+from pvanalytics.quality import time         # noqa: F401
+from pvanalytics.quality import data_shifts  # noqa: F401
