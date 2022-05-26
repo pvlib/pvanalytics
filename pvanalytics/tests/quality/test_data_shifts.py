b0223811067cab3d612e8ae0dca328902cf75c53
Kirsten Perry <kperry@nrel.gov>
2022-05-26 14:16:03 -0600

    renamed variables in the test file for data shifts from time_series to series

--- pvanalytics/tests/quality/test_data_shifts.py
+++ pvanalytics/tests/quality/test_data_shifts.py
@@ -10,7 +10,7 @@ test_file_1 = DATA_DIR / "pvlib_data_shift.csv"
 
 
 @pytest.fixture
-def generate_time_series():
+def generate_series():
     # Pull down the saved PVLib dataframe and process it
     df = pd.read_csv(test_file_1)
     signal_no_index = df['value']
@@ -23,13 +23,13 @@ def generate_time_series():
 
 
 @requires_ruptures
-def test_detect_data_shifts(generate_time_series):
+def test_detect_data_shifts(generate_series):
     """
     Unit test that data shifts are correctly identified in the simulated time
     series.
     """
     signal_no_index, signal_datetime_index, df_weekly_resample, \
-        changepoint_date = generate_time_series
+        changepoint_date = generate_series
     # Test that an error is thrown when a Pandas series with no datetime
     # index is passed
     pytest.raises(TypeError, dt.detect_data_shifts, signal_no_index)
@@ -44,7 +44,7 @@ def test_detect_data_shifts(generate_time_series):
                   True, False, ruptures.Binseg, "none")
     # Test that a data shift is successfully detected within 5 days of
     # inserted changepoint
-    shift_index = dt.detect_data_shifts(time_series=signal_datetime_index)
+    shift_index = dt.detect_data_shifts(series=signal_datetime_index)
     shift_index_dates = list(shift_index[shift_index].index)
     # Test that the column name is handled if a series with no name is passed
     signal_unnamed = signal_datetime_index.rename(None)
@@ -62,19 +62,19 @@ def test_detect_data_shifts(generate_time_series):
     assert (len(shift_index_param.index) == len(signal_datetime_index.index))
 
 
-def test_get_longest_shift_segment_dates(generate_time_series):
+def test_get_longest_shift_segment_dates(generate_series):
     """
     Unit test that the longest interval between data shifts is selected for
     the simulated daily time series data set.
     """
     signal_no_index, signal_datetime_index, df_weekly_resample, \
-        changepoint_date = generate_time_series
+        changepoint_date = generate_series
     # Run the time series where there are no changepoints
     interval_dict_short = dt.get_longest_shift_segment_dates(
-        time_series=signal_datetime_index[:100])
+        series=signal_datetime_index[:100])
     # Run the time series where there is a changepoint
     interval_dict = dt.get_longest_shift_segment_dates(
-        time_series=signal_datetime_index)
+        series=signal_datetime_index)
     assert (interval_dict['start_date'] == pd.to_datetime('2015-11-06')) & \
         (interval_dict['end_date'] == pd.to_datetime('2020-12-24'))
     assert (interval_dict_short['start_date'] ==
