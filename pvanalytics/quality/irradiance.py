d32c4c972172c925bdb6d576086ef5a55d49ca99
Kevin Anderson <kevin.anderson@nrel.gov>
2022-11-14 18:27:32 -0500

    think I did this right

--- pvanalytics/quality/irradiance.py
+++ pvanalytics/quality/irradiance.py
@@ -621,7 +621,7 @@ def calculate_component_sum_series(solar_zenith,
         Pandas series of clearsky dni data. Must have the same datetime index
         as `ghi`, `dhi`, `dni`, and `solar_zenith`, when available. See
         pvlib-python's 
-        [dni](https://pvlib-python.readthedocs.io/en/stable/reference/generated/pvlib.irradiance.dni.html#pvlib.irradiance.dni) for details.
+        `dni <https://pvlib-python.readthedocs.io/en/stable/reference/generated/pvlib.irradiance.dni.html#pvlib.irradiance.dni>`_ for details.
     zenith_limit: Float
         Solar zenith boundary between night and day, in degrees.
         For calculation of the component sum, `solar_zenith` is set to 90 where
