b2fd08bf9729f79541e15e7fead0418ddac5d7de
Perry, Kirsten <Kirsten.Perry@nrel.gov>
2023-08-09 13:12:20 -0600

    more variable renaming

--- pvanalytics/quality/time.py
+++ pvanalytics/quality/time.py
@@ -1,7 +1,6 @@
 """Quality tests related to time."""
 import warnings
 import pandas as pd
-from scipy import stats
 from pvanalytics.quality.outliers import zscore
 
 
@@ -167,7 +166,7 @@ def shifts_ruptures(event_times, reference_times,
         segment = segment[
             (segment >= segment.quantile(bottom_quantile_threshold)) &
             (segment <= segment.quantile(top_quantile_threshold))]
-        shift_amount.loc[index: index + 1] = minute_rounding * \
+        shift_amount.loc[index: index + 1] = shift_min * \
             round(float(segment.mean())/shift_min)
     # localize the shift_amount series to the timezone of the input
     shift_amount = shift_amount.tz_localize(event_times.index.tz)
