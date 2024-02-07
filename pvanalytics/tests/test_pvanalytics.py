ab33fd9026d6760356f4faa0f1e72e0193602229
Kevin Anderson <kevin.anderso@gmail.com>
2024-02-07 16:04:09 -0500

    don't use pkg_resources anymore

--- pvanalytics/tests/test_pvanalytics.py
+++ pvanalytics/tests/test_pvanalytics.py
@@ -1,5 +1,5 @@
 
-from pkg_resources import parse_version
+from packaging.version import Version
 import pvanalytics
 
 
@@ -7,5 +7,5 @@ def test___version__():
     # check that the version string is determined correctly.
     # if the version string is messed up for some reason, it should be
     # '0+unknown', which is not greater than '0.0.1'.
-    version = parse_version(pvanalytics.__version__)
-    assert version > parse_version('0.0.1')
+    version = Version(pvanalytics.__version__)
+    assert version > Version('0.0.1')
